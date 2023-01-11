d70de3acc7214b7683d1efb47f507bf0993357c2
zxielle <@JahazielFaye>
2023-01-11 13:45:33 -0500

    structuring files and adding routing

--- Develop/server.js
+++ Develop/server.js
@@ -17,8 +17,68 @@ app.use(express.urlencoded({ extended: true }));
 //Allows us to go through public folder
 app.use(express.static('public'));
 
-//Modular Routing here <---->
-app.get('/htmlnotes', (req, res)=> {
-    res.sendFile(path.join(__dirname, '.'))
+//GET Modular Routing here <---->
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
 })
+
+app.get('/notes', (req, res)=> {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('/api/notes', (req,res)=> {
+    res.json(dbJ)
+});
+
+//POST Routing here <-------->
+app.post('/api/notes', (req, res) =>{
+    //Log our request to terminal
+    console.info(`${req.method} request has been received`);
+
+    //destructuring
+    const { title, text } = req.body;
+
+    if(title && text) {
+        const newNote = {
+            title, 
+            text,
+            id: uniqid(),
+        };
+
+        //loads the existing notes
+        fs.readFile('./db/db.json', 'utf8', (err, data) =>{
+            if(err) {
+                console.log(err);
+            } else {
+                const parsedData = JSON.parse(data);
+
+                //to input new notes
+                parsedData.push(newNote);
+
+                //add new notes to db folder
+                fs.writeFile('./db/db.json', JSON.stringify(parsedData), null, (err) =>{
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        console.log('New Note Added Successfully!');
+                    }
+                })
+            }
+        })
+    }
+});
+ const response = {
+    status: 'Success',
+    body: newNote,
+
+ }
+ 
+ console.log(response);
+ res.status(201),json(response);
+} else {
+
+}
+
+
+
 
